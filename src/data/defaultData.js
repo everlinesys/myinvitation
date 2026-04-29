894977c552ec3f12121affd350b6f7227b959872
Azhar <azhar.subair.2@gmail.com>
2026-04-29 11:47:35 +0530

    test

--- src/data/defaultData.js
+++ src/data/defaultData.js
@@ -5,8 +5,8 @@ export const defaultData = {
   groomParents: "",
   brideParents: "",
 
-  date: "12 December 2026",
-  time: "10:30 AM",
+  date: "10 June 2026",
+  time: "09:00 AM",
   venue: "Kochi, Kerala",
 
   message: "With the blessings of our families, we invite you to celebrate our wedding.",
